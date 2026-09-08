a58b1d51ec12446df2bd289c3d5786ebf9bb3b22
AlexDev2004 <alexis.j2359@gmail.com>
2026-09-08 16:44:32 +0200

    Ajout du système de tri des produits

--- js/products.js
+++ js/products.js
@@ -30,18 +30,52 @@ ajoutProduit.addEventListener("submit",()=>{
     const categorieProduit = document.getElementById("categorie");
     const uniteProduit = document.getElementById("unite");
 
-    produitsTab.push(new Produits(nomProduit.value,categorieProduit.value,uniteProduit.value,prixProduit.value));
+    produitsTab.push(new Produits(nomProduit.value,categorieProduit.value,uniteProduit.value,Number(prixProduit.value)));
 
     popUp.classList.remove("articleOn");
 
     localStorage.setItem("productTab",JSON.stringify(produitsTab));
 });
 
+// ----------------------------------- Sélection du filtre de recherche ----------------------------
+
+const filtreRecherche = document.getElementById("filterProductTypeSearch");
+filtreRecherche.value = localStorage.getItem("filtreRecherche");
+
+filtreRecherche.addEventListener("change",()=>{
+    localStorage.setItem("filtreRecherche",filtreRecherche.value);
+    location.reload();
+});
+
 // ------------------------- Insertion des lignes de produits dans le tableau ----------------------
-
-
 const tableau = document.querySelector("table");
-
+// --------------------------------- Tris du tableau ---------------------------
+if(filtreRecherche.value == "alphabetique"){
+    produitsTab.sort((a, b) =>
+        a.nom.localeCompare(b.nom, undefined, {
+            sensitivity: "base"
+        })
+    );
+}
+if(filtreRecherche.value == "catégorie"){
+    produitsTab.sort((a, b) =>
+        a.categorie.localeCompare(b.categorie, undefined, {
+            sensitivity: "base"
+        })
+    );
+}
+if(filtreRecherche.value == "unite"){
+    produitsTab.sort((a, b) =>
+        a.unite.localeCompare(b.unite, undefined, {
+            sensitivity: "base"
+        })
+    );
+}
+if(filtreRecherche.value == "prix"){
+    produitsTab.sort((a, b) => b.prix - a.prix);
+}
+localStorage.setItem("productTab",JSON.stringify(produitsTab));
+// ------------------------------------------------------------------------------
 produitsTab.map((produit)=>{
     const ligneProduit = document.createElement("tr");
 
@@ -53,9 +87,58 @@ produitsTab.map((produit)=>{
     uniteProduit.innerHTML = produit.unite;
     const prixProduit = document.createElement("td");
     prixProduit.innerHTML = produit.prix;
-
-    ligneProduit.append(nomProduit,categorieProduit,uniteProduit,prixProduit);
+    const colonneAction = document.createElement("td");
+    colonneAction.innerHTML = `<svg class="pen" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--cButton)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pen"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/></svg>
+    <svg class="trash" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--red1)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>`
+    ligneProduit.append(nomProduit,categorieProduit,uniteProduit,prixProduit,colonneAction);
     tableau.appendChild(ligneProduit);
-
-    console.log(produit);
 });
+
+
+const pen = document.querySelectorAll(".pen");
+const penTab = Array.from(pen);
+penTab.map((pen,i)=>{
+    pen.addEventListener("click",()=>{
+        popUp.classList.add("articleOn");
+
+        const ligneSelect = document.querySelectorAll("tr")[i+1];
+
+        const colName = ligneSelect.querySelectorAll("td")[0];
+        const colCategorie = ligneSelect.querySelectorAll("td")[1];
+        const colUnite = ligneSelect.querySelectorAll("td")[2];
+        const colPrice = ligneSelect.querySelectorAll("td")[3];
+
+        const nomProduit = document.getElementById("nomProduitText");
+        nomProduit.value = colName.innerHTML;
+        const categorieProduit = document.getElementById("categorie");
+        categorieProduit.value = colCategorie.innerHTML;
+        const uniteProduit = document.getElementById("unite");
+        uniteProduit.value = colUnite.innerHTML;
+        const prixProduit = document.getElementById("prixU");
+        prixProduit.value = colPrice.innerHTML;
+
+        produitsTab.splice(i,1);
+    });
+});
+
+const trash = document.querySelectorAll(".trash");
+const trashTab = Array.from(trash);
+const overlaySuppr = document.querySelector(".overlaySuppr");
+
+let ligneSelectSuppr;
+
+trashTab.map((trash,i)=>{
+    trash.addEventListener("click",()=>{
+    overlaySuppr.classList.add("overlaySupprClicked");
+    ligneSelectSuppr = i;
+    });
+});
+
+const boutonOui = overlaySuppr.querySelectorAll("button")[0];
+const boutonNon = overlaySuppr.querySelectorAll("button")[1];
+
+boutonOui.addEventListener("click",()=>{
+    produitsTab.splice(ligneSelectSuppr,1);
+    localStorage.setItem("productTab",JSON.stringify(produitsTab));
+});
+
