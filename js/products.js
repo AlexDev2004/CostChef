68664434ec0d3fcdd8861409520989cd1220028d
AlexDev2004 <alexis.j2359@gmail.com>
2026-09-10 15:30:22 +0200

    files

--- js/products.js
+++ js/products.js
@@ -88,12 +88,17 @@ produitsTab.map((produit)=>{
     const prixProduit = document.createElement("td");
     prixProduit.innerText = produit.prix;
     const colonneAction = document.createElement("td");
-    colonneAction.innerHTML = `<svg class="pen" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--cButton)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pen"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/></svg>
-    <svg class="trash" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--red1)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>`
+    colonneAction.innerHTML = `<svg class="pen" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" 
+    stroke="var(--cButton)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pen">
+    <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/></svg>
+    <svg class="trash" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--red1)" stroke-width="2" 
+    stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/>
+    <path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>`
     ligneProduit.append(nomProduit,categorieProduit,uniteProduit,prixProduit,colonneAction);
     tableau.appendChild(ligneProduit);
 });
 
+// ------------------------ Modification et suppession ---------------------------------
 
 const pen = document.querySelectorAll(".pen");
 const penTab = Array.from(pen);
